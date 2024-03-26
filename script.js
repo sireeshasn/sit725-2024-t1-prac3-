6b5be27fbc8eebc8efce9268c66592f5434b5d2c
Sireesha Akurathi <sireesha2622@gmail.com>
2024-03-26 16:26:02 +1100

    none

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
-const clickMe=()=>{
-    alert("thanks for clicking me.Hope you have a nice day!")
-}
 $(document).ready(function(){
-    $('.materialboxed').materialbox();
-    $('#clickMeButton').click(()=>{
-        clickMe();
-    })
+    // Initialize modal
+    $('.modal').modal();
+
+    // Attach click event handler to open modal when "click me" button is clicked
+    $('#clickMeButton').click(function() {
+        $('#modal1').modal('open');
+    });
+
+    // Handle form submission
+    $('#formSubmit').click(function() {
+        // Here, you can add your form submission logic
+        // For example, you can retrieve form values using jQuery and perform AJAX request
+        var firstName = $('#first_name').val();
+        var lastName = $('#last_name').val();
+        var password = $('#password').val();
+        var email = $('#email').val();
+
+        // Perform form submission or any other desired action
+        // For demonstration, let's just log the values to the console
+        console.log("First Name:", firstName);
+        console.log("Last Name:", lastName);
+        console.log("Password:", password);
+        console.log("Email:", email);
+
+        // Close the modal after form submission
+        $('#modal1').modal('close');
+    });
 });
