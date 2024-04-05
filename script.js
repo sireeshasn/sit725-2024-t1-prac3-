f4af7041130b8674dbf634e401f6268753187689
Sireesha Akurathi <sireesha2622@gmail.com>
2024-04-05 11:39:30 +1100

    commit

--- script.js
+++ script.js
@@ -1,29 +1,40 @@
 $(document).ready(function(){
-    // Initialize modal
+ 
     $('.modal').modal();
 
-    // Attach click event handler to open modal when "click me" button is clicked
     $('#clickMeButton').click(function() {
         $('#modal1').modal('open');
     });
 
-    // Handle form submission
+  
     $('#formSubmit').click(function() {
-        // Here, you can add your form submission logic
-        // For example, you can retrieve form values using jQuery and perform AJAX request
         var firstName = $('#first_name').val();
         var lastName = $('#last_name').val();
         var password = $('#password').val();
         var email = $('#email').val();
 
-        // Perform form submission or any other desired action
-        // For demonstration, let's just log the values to the console
-        console.log("First Name:", firstName);
-        console.log("Last Name:", lastName);
-        console.log("Password:", password);
-        console.log("Email:", email);
+        var formData = {
+            firstName: firstName,
+            lastName: lastName,
+            password: password,
+            email: email
+        };
 
-        // Close the modal after form submission
+        
+        $.ajax({
+            type: 'POST',
+            url: '/api/cats', 
+            data: formData,
+            success: function(response) {
+                console.log(response); 
+                
+                $('#catPostSuccess').text('Cat post successful');
+            },
+            error: function(error) {
+                console.error('Error:', error); 
+               
+            }
+        });
         $('#modal1').modal('close');
     });
 });
